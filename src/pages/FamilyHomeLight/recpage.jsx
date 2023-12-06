728435364bc4861478501acf524b8a1d46bd4333
svng-zu <zztisdudoo@gmail.com>
2023-12-07 03:00:24 +0900

    내일 하자..

--- src/pages/FamilyHomeLight/recpage.jsx
+++ src/pages/FamilyHomeLight/recpage.jsx
@@ -19,11 +19,8 @@ function FamilyHomeLightPage() {
 
         const response = await axios.get('https://hello00back.net/vodrec/', {
           headers: {
-            Authorization: `Bearer${access}`,
-            
+            Authorization : `Bearer ${access}`,
           },
-          //withCredentials: true,
-          // 인증 정보를 함께 보내도록 설정
         });
 
         const data = response.data.data;
