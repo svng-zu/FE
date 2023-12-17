0aff28a8299fb730abe0fe0e3eed3f655484645b
svng-zu <zztisdudoo@gmail.com>
2023-12-18 01:36:43 +0900

    for banner

--- src/components/Firtstlist/index.jsx
+++ src/components/Firtstlist/index.jsx
@@ -65,7 +65,7 @@ const Firstlist = () => {
                 </div>
 
             {/* <div className="relative image-slider-wrapper"> */}
-            <div className="absolute image-slider m-auto">
+            <div className="absolute image-slider ml-[45%]">
                 
                 <div className="image-with-text">
 
