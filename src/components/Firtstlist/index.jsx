0b2b8bff0c2a35b9369b6f3421f3554eac7f791d
svng-zu <zztisdudoo@gmail.com>
2023-12-18 01:47:29 +0900

    whywy

--- src/components/Firtstlist/index.jsx
+++ src/components/Firtstlist/index.jsx
@@ -65,7 +65,7 @@ const Firstlist = () => {
                 </div>
 
             {/* <div className="relative image-slider-wrapper"> */}
-            <div className="absolute image-slider ml-[25%]">
+            <div className="absolute image-slider ml-[15%]">
                 
                 
 
